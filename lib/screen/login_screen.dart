d697fcac498828cfecbcfe66d84b9d207561713f
揉揉人人 <rrrr.zhao@qq.com>
2023-11-14 10:57:41 +0800

    重置密码逻辑，后台手动将密码清空后重新注册，如果昵称和手机号验证无误，密码将重置

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -79,43 +79,99 @@ class LoginScreenState extends State<LoginScreen> {
             .add(const Duration(days: 50))
             .toString()
             .substring(0, 10);
-
-        if (results.isNotEmpty) {
+        if (results.isNotEmpty && results[0][5] != '') {
           return '该用户已存在  请登录';
-        } else {
+        } else if (results.isEmpty) {
           var tmp = sha512.convert(utf8.encode('${data.password}IceBergNote'));
           int back = await connection!.execute(
-              "INSERT INTO userinfo (id, name, email, password, isadmin, createtime, viptime) VALUES (${no[0][0]}, '${data.name}', '${data.name}', '$tmp', 'f', '$date1', '$date2')");
+              "INSERT INTO userinfo (id, name, email, password, isadmin, createtime, viptime, phone) VALUES (${no[0][0]}, '${data.additionalSignupData!['昵称'] ?? ''}', '${data.name}', '$tmp', 'f', '$date1', '$date2','${data.additionalSignupData!['手机'] ?? ''}')");
           if (back == 1) {
             final SharedPreferences userLocalInfo =
                 await SharedPreferences.getInstance();
-            userLocalInfo.setString('userName', data.name!);
+            userLocalInfo.setString(
+                'userName', data.additionalSignupData!['昵称'] ?? '');
             userLocalInfo.setString('userID', no[0][0].toString());
             userLocalInfo.setString('userEmail', data.name!);
-            userLocalInfo.setString('userPhone', '');
+            userLocalInfo.setString(
+                'userPhone', data.additionalSignupData!['手机'] ?? '');
             userLocalInfo.setBool('userIsAdmin', false);
             userLocalInfo.setString('userCreatDate', date1);
             userLocalInfo.setString('userVIPDate', date2);
             Get.find<UserController>().refreshLocalUser();
-            return null;
           } else {
             return '注册失败';
           }
+        } else {
+          if (data.additionalSignupData!['昵称'] == results[0][0] &&
+              data.additionalSignupData!['手机'] == results[0][3]) {
+            var tmp =
+                sha512.convert(utf8.encode('${data.password}IceBergNote'));
+            int back = await connection!.execute(
+                "UPDATE userinfo SET password='$tmp' WHERE id='${results[0][1]}'");
+            if (back == 1) {
+              final SharedPreferences userLocalInfo =
+                  await SharedPreferences.getInstance();
+              userLocalInfo.setString('userName', results[0][0]);
+              userLocalInfo.setString('userID', no[0][0].toString());
+              userLocalInfo.setString('userEmail', data.name!);
+              userLocalInfo.setString(
+                  'userPhone', data.additionalSignupData!['手机'] ?? '');
+              userLocalInfo.setBool('userIsAdmin', false);
+              userLocalInfo.setString('userCreatDate', date1);
+              userLocalInfo.setString('userVIPDate', date2);
+              Get.find<UserController>().refreshLocalUser();
+              Get.toNamed('/');
+            } else {
+              return '重置密码失败';
+            }
+          } else {
+            return '用户验证错误';
+          }
+          // var tmp = sha512.convert(utf8.encode('${data.password}IceBergNote'));
+          // int back = await connection!.execute(
+          //     "UPDATE userinfo SET name=${data.additionalSignupData!['昵称'] ?? ''}, password=$tmp, phone=${data.additionalSignupData!['手机'] ?? ''}");
+          // if (back == 1) {
+          //   final SharedPreferences userLocalInfo =
+          //       await SharedPreferences.getInstance();
+          //   userLocalInfo.setString(
+          //       'userName', data.additionalSignupData!['昵称'] ?? '');
+          //   userLocalInfo.setString('userID', no[0][0].toString());
+          //   userLocalInfo.setString('userEmail', data.name!);
+          //   userLocalInfo.setString(
+          //       'userPhone', data.additionalSignupData!['手机'] ?? '');
+          //   userLocalInfo.setBool('userIsAdmin', false);
+          //   userLocalInfo.setString('userCreatDate', date1);
+          //   userLocalInfo.setString('userVIPDate', date2);
+          //   Get.find<UserController>().refreshLocalUser();
+          //   return null;
+          // } else {
+          //   return '注册失败';
+          // }
         }
       }
     } catch (e) {
       await Future.delayed(const Duration(seconds: 5));
       return '网络错误';
     }
+    return null;
   }
 
-  Future<String?> _recoverPassword(String name) {
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return '用户不存在';
-      }
-      return null;
-    });
+  Future<String?> _recoverPassword(String name) async {
+    return '请手动发送邮件';
+  }
+
+  static String? emailValidator(String? value) {
+    if (value == null || value.isEmpty || !Regex.email.hasMatch(value)) {
+      return '无效的邮箱';
+    }
+    return null;
+  }
+
+  static String? passwordValidator(String? value) {
+    if (value == null || value.isEmpty || value.length <= 2) {
+      return '密码太短';
+    }
+    return null;
   }
 
   @override
@@ -126,9 +182,15 @@ class LoginScreenState extends State<LoginScreen> {
       onLogin: _authUser,
       onSignup: _signupUser,
       onSubmitAnimationCompleted: () {
-        Navigator.pushNamed(context, '/');
+        Get.toNamed('/');
       },
       onRecoverPassword: _recoverPassword,
+      passwordValidator: passwordValidator,
+      userValidator: emailValidator,
+      additionalSignupFields: const [
+        UserFormField(keyName: '昵称'),
+        UserFormField(keyName: '手机')
+      ],
       messages: LoginMessages(
         userHint: '邮箱',
         passwordHint: '密码',
@@ -138,14 +200,18 @@ class LoginScreenState extends State<LoginScreen> {
         signupButton: '注册',
         recoverPasswordButton: '恢复密码',
         confirmPasswordError: '密码不一致',
-        recoverPasswordSuccess: '恢复密码成功',
+        recoverPasswordSuccess: '请手动发送邮件',
         goBackButton: '返回',
         resendCodeButton: '重新发送',
         resendCodeSuccess: '发送成功',
-        recoverPasswordDescription: '我们将会给这个邮箱发送明文密码',
+        recoverPasswordDescription:
+            '请用该注册邮箱给rrrr.zhao@qq.com发送重置密码邮件，稍后您将收到人工回复邮件',
         recoverPasswordIntro: '重置密码',
         flushbarTitleError: '错误',
         flushbarTitleSuccess: '成功',
+        additionalSignUpFormDescription: '建议您完善以下信息',
+        additionalSignUpSubmitButton: '提交',
+        signUpSuccess: '恭喜您注册成功',
       ),
       theme: LoginTheme(
         titleStyle: const TextStyle(color: Colors.white),
@@ -159,4 +225,9 @@ class LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+}
+
+class Regex {
+  // https://stackoverflow.com/a/32686261/9449426
+  static final email = RegExp(r'^[^\s@]+@[^\s@]+\.[^\s@]+$');
 }
