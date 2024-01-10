5e3f07bc9e833738205c371d7ef650a9d3ea2aa4
揉揉人人 <rrrr.zhao@qq.com>
2024-01-10 20:59:46 +0800

    1

---
+++ lib/system/keyword.dart
@@ -0,0 +1,9 @@
+Map<String, List<String>> recordKeyword = {
+  '表单': ['表单', 'header'],
+  '清单': ['清单', 'checklist'],
+  '记录': ['记录', 'record'],
+  '待办': ['待办', 'todo'],
+  '未完': ['未完', 'unfinished'],
+  '已完': ['已完', 'done'],
+  '模板': ['模板', 'template'],
+};
